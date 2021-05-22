3fc024407c39c81e4bac4a73d279ab8e5c91a5a3
Moskvin Pasha <79521676+Moskvin23@users.noreply.github.com>
2021-05-22 13:04:26 +0300

    Add files via upload

---
+++ Calculator, with dark theme/app.js
@@ -0,0 +1,69 @@
+let body = document.querySelector('body');
+let result = document.querySelector('#result');
+
+
+let dark_mode_btn = document.querySelector('.dark_mode_btn');
+let clear = document.querySelector('#clear');
+let history = document.querySelector('#history');
+let equalTo = document.querySelector('#equalTo');
+let delete_single_num = document.querySelector("#delete_single_num");
+
+let Normal_btn = document.querySelectorAll('#Normal_btn');
+
+
+let initial_value ="";
+
+Normal_btn.forEach((Normal_btn, index) =>{
+Normal_btn.addEventListener('click',function(){
+let text = this.innerHTML;
+initial_value += text;
+result.innerHTML = initial_value;
+});
+});
+
+// equal to button action
+
+equalTo.addEventListener('click',function(){
+if(result.innerHTML !=""){
+    history.innerHTML = result.innerHTML;
+    result.innerHTML = eval(result.innerHTML);
+    initial_value = eval(result.innerHTML);
+}else{
+    alert('please enter any Number');
+}
+});
+
+
+// clear all number  /C/
+
+clear.addEventListener('click',function(){
+    result.innerHTML = "";
+    initial_value = "";
+});
+
+
+//dark mode
+
+let dark_mode_status = false;
+dark_mode_btn.addEventListener('click',function(){
+body.classList.toggle('dark_mode_active');
+if (dark_mode_status == false) {
+    this.innerHTML = '<i class="far fa-sun"></i>';
+    dark_mode_status = true;;
+}else{
+    this.innerHTML = '<i class="fal fa-moon" ></i>';
+    dark_mode_status = false;
+}
+});
+
+
+//delete single number
+
+delete_single_num.addEventListener('click', function(){
+result.innerHTML = result.innerHTML.substring(0,result.innerHTML.length-1);
+initial_value = result.innerHTML;
+});
+
+
+
+
